a433d70779fedf27932ab582659c626063b39f25
JullyLoureiro <juliana.ferreira@soulasalle.com.br>
2019-05-12 21:59:48 -0300

    pesquisa de gráficos

--- app/src/main/java/br/com/juliana/loureiro/projetofinalahp/Activity/SobreAHP.java
+++ app/src/main/java/br/com/juliana/loureiro/projetofinalahp/Activity/SobreAHP.java
@@ -1,14 +1,36 @@
 package br.com.juliana.loureiro.projetofinalahp.Activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 import br.com.juliana.loureiro.projetofinalahp.R;
 
 public class SobreAHP extends AppCompatActivity {
+    private RadarChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +38,106 @@ public class SobreAHP extends AppCompatActivity {
         setContentView(R.layout.activity_sobre_ahp);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+       /* ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 5));
+        entries.add(new BarEntry(6f, 1));
+        entries.add(new BarEntry(5f, 2));
+        entries.add(new BarEntry(7f, 3));
+
+        BarDataSet dataset = new BarDataSet(entries, "legenda");
+
+        BarChart chart = new BarChart(this);
+        chart.setDrawGridBackground(false);
+        chart.setDrawBarShadow(false);
+        chart.setDrawGridBackground(false);
+        chart.setFitBars(false);
+        chart.setHighlightFullBarEnabled(false);
+
+        setContentView(chart);
+
+        BarData data = new BarData(dataset);
+        chart.setData(data);
+
+        Description description = new Description();
+        description.setText("");
+        chart.setDescription(description);
+
+        dataset.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        XAxis xAxis = chart.getXAxis();
+       // xAxis.setGranularity(1f);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setDrawGridLines(false);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawLabels(true);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setDrawLabels(false);
+
+        chart.animateY(1000);
+        chart.invalidate();*/
+        //chart.saveToGallery("mychart.jpg", 85);
+        //<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+
+
+        chart = findViewById(R.id.chart);
+        chart.setBackgroundColor(Color.rgb(60, 65, 82));
+
+        chart.getDescription().setEnabled(false);
+
+        chart.setWebLineWidth(1f);
+        chart.setWebColor(Color.LTGRAY);
+        chart.setWebLineWidthInner(1f);
+        chart.setWebColorInner(Color.LTGRAY);
+        chart.setWebAlpha(100);
+
+        // create a custom MarkerView (extend MarkerView) and specify the layout
+        // to use for it
+        MarkerView mv = new RadarMarkerView(this, R.layout.radar_markerview);
+        mv.setChartView(chart); // For bounds control
+        chart.setMarker(mv); // Set the marker to the chart
+
+        setData();
+
+        chart.animateXY(1400, 1400, Easing.EaseInOutQuad);
+
+        XAxis xAxis = chart.getXAxis();
+      //  xAxis.setTypeface(tfLight);
+        xAxis.setTextSize(9f);
+        xAxis.setYOffset(0f);
+        xAxis.setXOffset(0f);
+        xAxis.setValueFormatter(new ValueFormatter() {
+
+            private final String[] mActivities = new String[]{"Burger", "Steak", "Salad", "Pasta", "Pizza"};
+
+            @Override
+            public String getFormattedValue(float value) {
+                return mActivities[(int) value % mActivities.length];
+            }
+        });
+        xAxis.setTextColor(Color.WHITE);
+
+        YAxis yAxis = chart.getYAxis();
+      //  yAxis.setTypeface(tfLight);
+        yAxis.setLabelCount(5, false);
+        yAxis.setTextSize(9f);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(80f);
+        yAxis.setDrawLabels(false);
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+      //  l.setTypeface(tfLight);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
+        l.setTextColor(Color.WHITE);
     }
 
     @Override
@@ -29,5 +151,56 @@ public class SobreAHP extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setData() {
+
+        float mul = 80;
+        float min = 20;
+        int cnt = 5;
+
+        ArrayList<RadarEntry> entries1 = new ArrayList<>();
+        ArrayList<RadarEntry> entries2 = new ArrayList<>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < cnt; i++) {
+            float val1 = (float) (Math.random() * mul) + min;
+            entries1.add(new RadarEntry(val1));
+
+            float val2 = (float) (Math.random() * mul) + min;
+            entries2.add(new RadarEntry(val2));
+        }
+
+        RadarDataSet set1 = new RadarDataSet(entries1, "Last Week");
+        set1.setColor(Color.rgb(103, 110, 129));
+        set1.setFillColor(Color.rgb(103, 110, 129));
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(180);
+        set1.setLineWidth(2f);
+        set1.setDrawHighlightCircleEnabled(true);
+        set1.setDrawHighlightIndicators(false);
+
+        RadarDataSet set2 = new RadarDataSet(entries2, "This Week");
+        set2.setColor(Color.rgb(121, 162, 175));
+        set2.setFillColor(Color.rgb(121, 162, 175));
+        set2.setDrawFilled(true);
+        set2.setFillAlpha(180);
+        set2.setLineWidth(2f);
+        set2.setDrawHighlightCircleEnabled(true);
+        set2.setDrawHighlightIndicators(false);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<>();
+        sets.add(set1);
+        sets.add(set2);
+
+        RadarData data = new RadarData(sets);
+        //ata.setValueTypeface(tfLight);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+        data.setValueTextColor(Color.WHITE);
+
+        chart.setData(data);
+        chart.invalidate();
     }
 }
